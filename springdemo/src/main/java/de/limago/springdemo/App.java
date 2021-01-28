65be662ce976f7a5d27c3e3fe0a384b6d1db985f
limagoHub <limagoHub@limago.de>
2021-01-28 10:25:22 +0100

    xx

--- springdemo/src/main/java/de/limago/springdemo/App.java
+++ springdemo/src/main/java/de/limago/springdemo/App.java
@@ -1,6 +1,6 @@
 package de.limago.springdemo;
 
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -13,10 +13,14 @@ public class App
 	
     public static void main( String[] args )
     {
-    	 AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-         HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-         obj.getMessage();
-         context.close();
+    	 ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+    	 
+    	 
+    	 System.out.println("xxxx");
+    	 
+         HelloWorld obj1 = (HelloWorld) context.getBean("helloWorld");
+        
+        obj1.getMessage();
          
     }
 }
--- springdemo/src/main/java/de/limago/springdemo/HelloWorld.java
+++ springdemo/src/main/java/de/limago/springdemo/HelloWorld.java
@@ -2,6 +2,10 @@ package de.limago.springdemo;
 
 public class HelloWorld {
 	private String message;
+	
+	public HelloWorld() {
+		System.out.println("ctor");
+	}
 
 	public void setMessage(String message) {
 		this.message = message;
